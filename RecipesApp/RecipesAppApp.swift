58c9c84c4fae215169c5fe9d8ac56a0f53969f7a
Michaellis <michael94ellis@gmail.com>
2025-05-31 13:14:32 -0400

    Initial Commit

---
+++ RecipesApp/RecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppApp.swift
+//  RecipesApp
+//
+//  Created by Michael Ellis on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipesAppTests/RecipesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppTests.swift
+//  RecipesAppTests
+//
+//  Created by Michael Ellis on 5/31/25.
+//
+
+import Testing
+@testable import RecipesApp
+
+struct RecipesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
